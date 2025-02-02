78244a567bfeb86c8b0a1b837809b9b99022bde7
Nikita Akatyev <frfntdakatevin@gmail.com>
2025-02-02 19:31:15 +0100

    chore: Add magic_enum and googletest via Conan

--- cmake/toolchains/system-clang-libcxx.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_CXX_COMPILER "clang++")
-set(CMAKE_CXX_FLAGS "-stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++")
-set(CMAKE_SHARED_LINKER_FLAGS "-stdlib=libc++")
---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps
+from conan import ConanFile
+
+class HelloConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "sanitize": [None, "Address", "Leak", "Memory", "Thread", "Undefined"],
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "sanitize": None,
+    }
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("magic_enum/0.9.7")
+        self.requires("gtest/1.12.1", test=True)
+        pass
+
+    def generate(self):
+        tc = CMakeToolchain(self, generator="Ninja")
+        tc.cache_variables["ENABLE_ADDRESS_SANITIZER"] = self.options.sanitize == "Address"
+        tc.cache_variables["ENABLE_LEAK_SANITIZER"] = self.options.sanitize == "Leak"
+        tc.cache_variables["ENABLE_MEMORY_SANITIZER"] = self.options.sanitize == "Memory"
+        tc.cache_variables["ENABLE_THREAD_SANITIZER"] = self.options.sanitize == "Thread"
+        tc.cache_variables["ENABLE_UNDEFINED_BEHAVIOUR_SANITIZER"] = self.options.sanitize == "Undefined"
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
